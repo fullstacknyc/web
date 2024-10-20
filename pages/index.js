f85a61ec0e0cec44d32770a955c731eb4af3ef43
fullstacknyc <myemailxd@icloud.com>
2024-10-20 14:19:07 -0400

    fixing app

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Homepage() {
-    return (
-        <div>
-            <h1>Welcome to My Website</h1>
-            <p>This is the homepage of my Next.js application.</p>
-        </div>
-    )
-}
