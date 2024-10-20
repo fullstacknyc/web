5edafba04ed9556a4d1a22886445be8d12e67a5a
fullstacknyc <myemailxd@icloud.com>
2024-10-20 14:12:05 -0400

    trying to get the page to load

---
+++ components/MyComponent.js
@@ -0,0 +1,11 @@
+// components/MyComponent.js
+
+export default function MyComponent() {
+    return (
+      <div>
+        <h2>This is My Component!</h2>
+        <p>You can reuse this component anywhere in your app.</p>
+      </div>
+    );
+  }
+  
